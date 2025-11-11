49374fe04dc19b555d43cc9bc4a21dc701777fa2
Florian Rohrer <semicolonator@gmail.com>
2025-11-11 19:49:49 +0100

    Quest 3 improvements

--- quest03/main.py
+++ quest03/main.py
@@ -1,6 +1,8 @@
+from collections import Counter
+
 # Part 1
 with open("everybody_codes_e2025_q03_p1.txt") as f:
-    vec = sorted(set([int(i) for i in f.read().split(",")]))
+    vec = set([int(i) for i in f.read().split(",")])
 print(sum(vec))
 
 # Part 2
@@ -9,8 +11,6 @@ with open("everybody_codes_e2025_q03_p2.txt") as f:
 print(sum(vec[:20]))
 
 # Part 3
-from collections import Counter
-
 with open("everybody_codes_e2025_q03_p3.txt") as f:
     vec = [int(i) for i in f.read().split(",")]
 
